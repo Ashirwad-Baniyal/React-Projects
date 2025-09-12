8df4b635a34dabe5629c21ea46227ee99f300d19
Ashirwad Baniyal <ashirwadbaniyal789@gmail.com>
2025-09-12 11:21:05 +0530

    V1 completed

--- reactRouter/src/App.jsx
+++ reactRouter/src/App.jsx
@@ -1,15 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
+import './App.css'
+import Footer from './components/footer/Footer'
+import Header from './components/header/Header'
+import Home from './components/home/Home'
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>
-      <h1 className='bg-green-600 p-4'>React router</h1>
-    </>
+      <Header/>
+      <Home/>
+      <Footer/>
+      </> 
   )
 }
 
--- reactRouter/src/main.jsx
+++ reactRouter/src/main.jsx
@@ -1,10 +1,63 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route
+} from "react-router-dom"; 
+import "./index.css";
+import Layout from "./Layout.jsx";
+import Home from "./components/home/Home.jsx";
+import Footer from "./components/footer/Footer.jsx";
+import About from "./components/about/About.jsx";
+import Contact from "./components/contact/Contact.jsx";
+import User from "./components/user/User.jsx";
+import Github, { githubInfoLoader } from './components/github/Github.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Layout />,
+//     children: [
+//       {
+//         path: "",
+//         element: <Home />,
+//       },
+//       {
+//         path: "about",
+//         element: <About />,
+//       },
+//       {
+//         path: "contact",
+//         element: <Contact />,
+//       },
+//     ],
+//   },
+// ]);
+
+const router=createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout/>}>
+      <Route path='' element={<Home/>}/>
+      <Route path='about' element={<About/>}/>
+      <Route path='contact' element={<Contact/>}/>
+      <Route path='user/:userid' element={<User />}/>
+      {/* <Route path='github' element={<Github />}/> */}
+      <Route 
+      loader={githubInfoLoader}
+      path='github' 
+      element={<Github />}
+       />
+
+    </Route>
+  )
 )
+
+
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
